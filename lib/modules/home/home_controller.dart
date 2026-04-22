e838c06840c062b1b8140da2e244444a5ce8bcdb
develper21 <githubnarvin@gmail.com>
2026-04-22 23:14:02 +0530

    feat: Add home controller for page navigation and state management

---
+++ lib/modules/home/home_controller.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeController extends ChangeNotifier {
+  ValueNotifier<int> currentPage = ValueNotifier<int>(0);
+  final pageController = PageController();
+
+  void setPage(int index) {
+    currentPage.value = index;
+    pageController.jumpToPage(index);
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    currentPage.dispose();
+    pageController.dispose();
+    super.dispose();
+  }
+}
